dc491edc0e6f8342eb44237408244770d85f8219
Colby Pate <cbpate08@gmail.com>
2022-08-18 12:09:49 -0500

    Add project files.

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "greater than or equal to nine";
+            //}
+            //Console.WriteLine(response);
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+        }
+    }
+}
